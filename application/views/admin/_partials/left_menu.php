77e964619435107f82b408212439fbf922a530c1
ram-izaap <ram.izaap@gmail.com>
2015-03-18 20:08:53 +0530

    lleft-menu issue fixed

--- application/views/admin/_partials/left_menu.php
+++ application/views/admin/_partials/left_menu.php
@@ -1,88 +1,82 @@
 
 <!-- Start  Left Menu Section -->
 <div id="nav-col">
-<section id="col-left" class="col-left-nano">
-<div id="col-left-inner" class="col-left-nano-content">
-<div class="collapse navbar-collapse navbar-ex1-collapse" id="sidebar-nav">
-<ul class="nav nav-pills nav-stacked">
-    <li >
-        <a href="<?php echo site_url('admin/dashboard'); ?>">
-            <i class="fa fa-dashboard"></i><span>Dashboard</span><span class="label label-primary label-circle pull-right"></span>
-        </a>
-    </li>
-    
-    <li>
-		<a href="<?php echo site_url('admin/role'); ?>">
-			<i class="fa fa-dashboard"></i><span>Role</span><span class="label label-primary label-circle pull-right"></span>
-		</a>
-		<ul class="submenu">
-			<li>
-				<a href="<?php echo site_url('admin/role/add_role');?>">
-					<i class=""></i><span>Add Role</span>
-				</a>
-			</li>
-			<li>
-				<a href="<?php echo site_url('admin/role/manage_role');?>">
-					<i class=""></i><span>Manage Role</span>
-				</a>
-			</li>
-		</ul>
-    </li>
+    <section id="col-left" class="col-left-nano">
+        <div id="col-left-inner" class="col-left-nano-content">
 
-    <li >
-		<a href="<?php echo site_url('admin/user'); ?>">
-			<i class="fa fa-dashboard"></i><span>Users</span><span class="label label-primary label-circle pull-right"></span>
-		</a>
-		<ul class="submenu">
-			<li>
-				<a href="<?php echo site_url('admin/user/add'); ?>">
-					<i class=""></i><span>Add User</span>
-				</a>
-			</li>
-			<li>
-				<a href="<?php echo site_url('admin/user'); ?>">
-					<i class=""></i><span>Manage Users</span>
-				</a>
-			</li>
-		</ul>
-    </li>
-    
-    
-    <li>
-        <a href="<?php echo site_url("admin/products"); ?>" >
-            <i class="fa fa-envelope"></i><span>Products</span>
-        </a>
-        <ul class="submenu">
-            <li>
-                <a href="<?php echo site_url("admin/products/add"); ?>">Add Products</a>
-            </li>
-            <li>
-                <a href="<?php echo site_url("admin/products"); ?>">Manage Products</a>
-            </li>
-        </ul>
-    </li>
-    
-    <li>
-		<a href="<?php echo site_url("admin/message"); ?>" >
-			<i class="fa fa-envelope"></i><span>Messages</span>
-		</a>
-		<ul class="submenu"> 
-			<li> 
-				<a href="<?php echo site_url("admin/message/add"); ?>" >
-					<i class=""></i><span>Add Mesaage</span>
-				</a>
-			</li>
-			<li>
-				<a href="<?php echo site_url("admin/message"); ?>" >
-					<i class=""></i><span>Message Management</span>
-				</a>
-			</li>
-		</ul>
-    </li>
-        
-</ul>
- </div>
- </div>
- </section>
+
+
+
+            <div class="collapse navbar-collapse navbar-ex1-collapse" id="sidebar-nav">
+                <ul class="nav nav-pills nav-stacked">
+                    
+                    <li >
+                        <a href="<?php echo site_url('admin/dashboard'); ?>">
+                            <i class="fa fa-dashboard"></i>
+                            <span>Dashboard</span>
+                            <span class="label label-primary label-circle pull-right"></span>
+                        </a>
+                    </li>
+
+                    <li >
+                        <a href="<?php echo site_url('admin/role'); ?>">
+                            <i class="fa fa-user"></i>
+                            <span>Role</span>
+                            <span class="label label-primary label-circle pull-right"></span>
+                        </a>
+                    </li>
+                    <li >
+                        <a href="<?php echo site_url('admin/user'); ?>">
+                            <i class="fa fa-users"></i>
+                            <span>Users</span>
+                            <span class="label label-primary label-circle pull-right"></span>
+                        </a>
+                    </li>
+
+                    <li >
+                        <a href="<?php echo site_url('admin/products'); ?>">
+                            <i class="fa fa-building"></i>
+                            <span>Products</span>
+                            <span class="label label-primary label-circle pull-right"></span>
+                        </a>
+                    </li>
+                    <li >
+                        <a href="<?php echo site_url('admin/message'); ?>">
+                            <i class="fa fa-envelope"></i>
+                            <span>Messages</span>
+                            <span class="label label-primary label-circle pull-right"></span>
+                        </a>
+                    </li>
+
+
+                    <!--<li>
+                        <a href="#" class="dropdown-toggle">
+                            <i class="fa fa-table"></i>
+                            <span>Tables</span>
+                            <i class="fa fa-angle-right drop-icon"></i>
+                        </a>
+                        <ul class="submenu">
+                            <li>
+                                <a href="tables.html">
+                                    Simple
+                                </a>
+                            </li>
+                            <li>
+                                <a href="tables-advanced.html">
+                                    Advanced
+                                </a>
+                            </li>
+                            <li>
+                                <a href="users.html">
+                                    Users
+                                </a>
+                            </li>
+                        </ul>
+                    </li>-->
+                    
+                </ul>
+            </div>
+        </div>
+    </section>
+    <div id="nav-col-submenu"></div>
 </div>
-<!-- End  Left Menu Section -->
